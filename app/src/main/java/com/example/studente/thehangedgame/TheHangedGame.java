a98c6797f6387b1d0445a63f006a7ab765a6f121
Enrico <enrifavaretto@gmail.com>
2018-02-20 12:57:18 +0100

    Initial commit

---
+++ app/src/main/java/com/example/studente/thehangedgame/TheHangedGame.java
@@ -0,0 +1,13 @@
+package com.example.studente.thehangedgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TheHangedGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the_hanged_game);
+    }
+}
